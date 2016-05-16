ac43545eff24ed4c496555cd16a98bbd6f7ab958
Susan Dreher <sdreher@columbia.edu>
2016-05-16 13:19:44 -0400

    /dist/ directory explictly referenced for svg/woff/ttf/eot loaders
    this seems necessary to output and load from the correct directory

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,28 @@ var path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: './dist/bundle.js'
     },
     module: {
         loaders: [
             {test: /\.css$/, loader: 'style!css!'},
             {test: /\.json/, loader: 'json-loader'},
-            {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+            {
+                test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff&name=./dist/[hash].[ext]"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream&name=./dist/[hash].[ext]"
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file?&name=./dist/[hash].[ext]" 
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml&name=./dist/[hash].[ext]"
+            }
         ]
     },
     jshint: {
