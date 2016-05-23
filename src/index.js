185dfae104f4f78d0f9e7fc41e5b909bea73bc94
Susan Dreher <sdreher@columbia.edu>
2016-05-23 11:15:38 -0400

    reorder css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /* global jQuery: true */
 
 // load css
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
 require('../static/css/common.css');
 require('../static/css/supportservices.css');
-require('../node_modules/bootstrap/dist/css/bootstrap.min.css');
 
 var jQuery = require('jquery');
 var module = require('./supportservices.js');
