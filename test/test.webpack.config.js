066f6037ca272a68e35232bd5034344dcb2348ea
Susan Dreher <sdreher@columbia.edu>
2016-06-06 10:48:47 -0400

    jscs/jshint the test files as well

--- test/test.webpack.config.js
+++ test/test.webpack.config.js
@@ -1,3 +1,5 @@
+/* global module: true */
+
 module.exports = {
     entry: './test/view-test.js',
     output: {
@@ -5,21 +7,24 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /\.html$/, loader: "underscore-template-loader"},
+            {test: /\.html$/, loader: 'underscore-template-loader'},
             {test: /\.css$/, loader: 'style!css!'},
             {test: /\.json/, loader: 'json-loader'}, {
                 test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=application/font-woff&name=./dist/[hash].[ext]"
+                loader: 'url?limit=10000&mimetype=application/font-woff' +
+                    '&name=./dist/[hash].[ext]'
             }, {
                 test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=application/octet-stream&name=./dist/[hash].[ext]"
+                loader: 'url?limit=10000&mimetype=application/octet-stream' +
+                    '&name=./dist/[hash].[ext]'
             }, {
                 test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "file?&name=./dist/[hash].[ext]" 
+                loader: 'file?&name=./dist/[hash].[ext]'
             }, {
                 test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url?limit=10000&mimetype=image/svg+xml&name=./dist/[hash].[ext]"
+                loader: 'url?limit=10000&mimetype=image/svg+xml' +
+                    '&name=./dist/[hash].[ext]'
             }
         ]
     }
-}
+};
