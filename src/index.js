76ff1ef841d42f051ea8e39216b29f4df8f1e5f9
zarina <zarina@columbia.edu>
2017-01-11 12:20:51 -0500

    Added logo paths definition

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /* global jQuery: true */
 
 require('!file?name=[name].[ext]!../static/index.html');
-require('!file?name=[name].[ext]!../static/img/ctl-logo.png');
+require('!file?name=[name].[ext]!../static/img/logo-ctl.png');
+require('!file?name=[name].[ext]!../static/img/logo-cdm.png');
 
 // load css
 require('!style!css!bootstrap/dist/css/bootstrap.min.css');
